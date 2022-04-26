52d29d9cbe4cd10300aa1ec1dd6702607b13b610
XanderKing <amking@usc.edu>
2022-04-26 16:51:15 -0700

    Final Project initial commit

--- project-3/script.js
+++ project-3/script.js
@@ -1,6 +1,166 @@
 $(document).ready(function () {
-    $(function () {
-        $(".i-want").draggable();
-        $(".i-should").draggable();
-    });   
+  $("#create-block").click(function () {
+    $("<div/>")
+      .attr({
+        class: "node",
+      })
+      .prepend("<span></span>")
+      .append("<span></span>")
+      .append("<span></span>")
+      .append("<div class='node-content'> </div>")
+      .append(
+        "<input type='text' name='input' autoComplete = 'off' placeholder='write it out'>"
+      )
+      //   .html(
+      //       "THOUGHT / DESIRE / TASK<br> <input type='text' name='input' placeholder='write it out'>"
+      //     )
+      .append('<button class="close-btn" type="button">X</button>')
+
+      .appendTo("#area")
+      .draggable({ containment: "window", scroll: false })
+      .css({
+        top:
+          Math.random() *
+            ($(window).innerHeight() - $(".node").height() * 1.5) +
+          "px", // multiply random .width(0..1) value by window height minus div height
+        left:
+          Math.random() *
+            ($(window).innerWidth() / 2 - $(".node").width() * 1.5) +
+          "px",
+      });
+
+    // $("<div/>")
+    // .attr({
+    //   class: "node",
+    // })
+    // .append(<span></span>)
+    // .append(<span></span>)
+    // .append(<span></span>)
+    // .html(
+    //   "THOUGHT / DESIRE / TASK<br> <input type='text' name='input' placeholder='write it out'>"
+    // )
+    // .append('<button class="close-btn" type="button">X</button>')
+    // .appendTo("#area")
+    // .draggable()
+    // .css({
+    //   top:
+    //     Math.random() *
+    //       ($(window).innerHeight() - $(".node").height() * 1.5) +
+    //     "px", // multiply random .width(0..1) value by window height minus div height
+    //   left:
+    //     Math.random() *
+    //       ($(window).innerWidth() / 2 - $(".node").width() * 1.5) +
+    //     "px",
+    // });
+  });
+
+  //   $('.close-btn').on('click', function(e) { $(e.target).closest('.draggable').hide(); });
+
+  $("body").on("click", ".close-btn", function () {
+    $(this).parent().remove();
+  });
+
+  var textarray = [
+    "What would make you happy?",
+    "What woud you tell your past self?",
+    // "You are in control!",
+    "You got this! :).",
+    "Just a reminder that you got this!",
+    "What works towards your goals?",
+    "Take care of yourself!",
+    "What would make you proud of yourself?",
+    // "Motivation and action are a loop, you can start anywhere!",
+    "Call a friend?",
+    "Try Again. Fail again. Fail better.", //Sam Levenson
+    "The secret of getting ahead is getting started.", //Mark Twain
+    "Perseverance is not a long race; it is many short races one after the other.", //Walter Elliot
+    "What you do today can improve all your tomorrows.", //Ralph Marston
+    "Resting is ok!",
+  ];
+
+  function RndText() {
+    var rannum = Math.floor(Math.random() * textarray.length);
+
+    $(".spawn-text").fadeOut("fast", function () {
+      $(this).text(textarray[rannum]).fadeIn("fast");
+    });
+  }
+
+  //   $(".draggable").draggable();
+  $(".node").draggable({ containment: "window", scroll: false });
+
+  $("#droppable").droppable({
+    drop: function () {
+      // $(".spawn-text").fadeIn();
+      RndText();
+    },
+  });
+
+  $("#bubble-1").on("click", function () {
+    $("#bubble-2").fadeIn(3000);
+  });
+
+  $("#bubble-2").on("click", function () {
+    $("#bubble-3").fadeIn(3000);
+  });
+
+  $("#bubble-3").on("click", function () {
+    $("#bubble-4").fadeIn(3000);
+  });
+
+  //   $("#bubble-4").on("click", function () {
+  //     $("#bubble-4").load("cerebrum.html");
+  //   });
 });
+
+function openFunction() {
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("mainbox").style.marginRight="350px";
+    document.getElementById("mainbox").innerHTML="<3 <br> &nbsp&nbsp&nbsp&nbsp <3 <br> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <3";
+
+}
+
+function closeFunction() {
+    document.getElementById("menu").style.width="0px";
+    document.getElementById("mainbox").style.marginRight="25px";
+    document.getElementById("mainbox").innerHTML ="&#9776; Open";
+
+}
+
+// document.getElementsByClassName('close-btn').addEventListener('click', function(e) {
+//     e.preventDefault();
+//     this.parentNode.style.display = 'none';
+// }, false);
+
+// document.querySelector("#tag").addEventListener("click", function(event) {
+//     // document.getElementById("tag").innerHTML += "Sorry! <code>preventDefault()</code> won't let you check this!<br>";
+//     event.preventDefault();
+// }, false);
+
+// window.onload = function(){
+//     document.getElementsByClassName('close-btn').onclick = function(){
+//         this.parentNode.parentNode.parentNode
+//         .removeChild(this.parentNode.parentNode);
+//         return false;
+//     };
+// };
+
+//   $("<div/>")
+//     .attr({
+//       class: "spawn-text",
+//       // <input type="text" name="input" placeholder="type something">
+//     })
+//     .html("What would make you happy?")
+//     .appendTo("#area")
+
+// .draggable()
+// .css({
+//   top:
+//     Math.random() *
+//       ($(window).innerHeight() - $(".spawn-text").height()) +
+//     "px", // multiply random .width(0..1) value by window height minus div height
+//   left:
+//     Math.random() *
+//       ($(window).innerWidth() - $(".spawn-text").width()) +
+//     "px",
+// });
--- project-3/sketch.js
+++ project-3/sketch.js
@@ -8,54 +8,53 @@ const noiseScale = 0.01;
 
 function setup() {
   var cnv = createCanvas(600, 600);
-  cnv.style('display', 'block');
+  cnv.style("display", "block");
   var x = (windowWidth - width) / 2;
   var y = (windowHeight - height) / 2 - 50;
-  cnv.position(x,y);
+  cnv.position(x, y);
 
-  for(let i = 0; i < num; i++) {
-    particles.push(createVector(random(width),330));
+  for (let i = 0; i < num; i++) {
+    particles.push(createVector(random(width), 330));
   }
   stroke(255);
-  lightX = width/2;
-  lightY = height/2;
+  lightX = width / 2;
+  lightY = height / 2;
 }
 
 function draw() {
-  background(0,10);
-  
-//   if(lightX == width){
-//     lightXforward = false;
-//   }
-  
-//   if(lightX == 0){
-//     lightXforward = true;
-//   }
-  
-//   if(lightXforward){
-//     lightX += 50;
-//   }else {
-//     lightX -= 50;
-//   }
+  background(0, 10);
 
+  //   if(lightX == width){
+  //     lightXforward = false;
+  //   }
 
-  for(let i = 0; i < num; i++){
+  //   if(lightX == 0){
+  //     lightXforward = true;
+  //   }
+
+  //   if(lightXforward){
+  //     lightX += 50;
+  //   }else {
+  //     lightX -= 50;
+  //   }
+
+  for (let i = 0; i < num; i++) {
     let p = particles[i];
-    stroke(255,255,255);
-    point(p.x,p.y);
+    stroke(255, 255, 255);
+    point(p.x, p.y);
     let n = noise(p.x * noiseScale, p.y * noiseScale);
     let a = TAU * n;
     p.x += cos(a) * d;
     p.y += sin(a) * d;
-    if(!onScreen(p)){
+    if (!onScreen(p)) {
       p.x = random(width);
       p.y = 330;
     }
   }
-  
+
   noiseSeed(12);
   d = -1;
-  
+
   // if(mouseX < 300){
   //   noiseSeed(1);
   //   d = 1;
@@ -63,7 +62,6 @@ function draw() {
   //   noiseSeed(2);
   //   d = -1;
   // }
-  
 }
 
 // function changeSeed(){
@@ -74,12 +72,12 @@ function onScreen(v) {
   return v.x >= 0 && v.x <= width && v.y >= 100 && v.y <= 600;
 }
 
-function centerCanvas(){
-    var x = (windowWidth - width) / 2;
-    var y = (windowHeight - height) / 2 - 50;
-    cnv.position(x,y);
-  }
-  
-  function windowResized() {
-    centerCanvas();
-  }
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2 - 50;
+  cnv.position(x, y);
+}
+
+function windowResized() {
+  centerCanvas();
+}
